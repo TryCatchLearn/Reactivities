856b098e8d562b2da7a5b578c179d152f30a7128
mahabaliaayush <44992429+mahabaliaayush@users.noreply.github.com>
2025-05-20 17:13:32 +0530

    End from aayush of section 7

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import mkcert from 'vite-plugin-mkcert';
-
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 3000
   },
-  plugins: [react(), mkcert()],
+  plugins: [react()],
 })
