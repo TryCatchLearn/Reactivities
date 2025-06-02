f478b3ff82202ef6bf15d410058eab0f1cfa70fe
mahabaliaayush <44992429+mahabaliaayush@users.noreply.github.com>
2025-06-02 18:43:24 +0530

    End of section 12 From ayush

--- API/Program.cs
+++ API/Program.cs
@@ -5,11 +5,19 @@ using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -18,21 +26,35 @@ builder.Services.AddCors();
 builder.Services.AddMediatR(x =>
 {
     x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>();
-    x.AddOpenBehavior( typeof (ValidationBehavior<,>));
+    x.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
-    
+
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
 builder.Services.AddTransient<ExceptoinMiddleware>();
+builder.Services.AddIdentityApiEndpoints<User>(opt =>
+{
+    opt.User.RequireUniqueEmail = true;
+
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptoinMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+
+    .AllowCredentials()
     .WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<User>();   //api/login
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
@@ -40,8 +62,9 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<AppDbContext>();
+    var UserManager = services.GetRequiredService<UserManager<User>>();
     await context.Database.MigrateAsync();
-    await DbInitializer.SeedData(context);
+    await DbInitializer.SeedData(context, UserManager);
 }
 catch (Exception ex)
 {
